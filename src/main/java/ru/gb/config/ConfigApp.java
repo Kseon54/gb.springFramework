ddd9b6b8df2efe5f6cc8a65a7b73bc0d16a7661d
Piven Vadim <vp29101998q@gmail.com>
2022-02-01 22:51:40 +0900

    base config

--- src/main/java/ru/gb/config/ConfigApp.java
+++ src/main/java/ru/gb/config/ConfigApp.java
@@ -1,10 +1,22 @@
 package ru.gb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("ru.gb")
 public class ConfigApp {
 
+    public final static String VIEW_RESOLVER_PREFIX="WEB-INF/view/";
+    public final static String VIEW_RESOLVER_SUFFIX=".jsp/";
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix(VIEW_RESOLVER_PREFIX);
+        viewResolver.setSuffix(VIEW_RESOLVER_SUFFIX);
+        return viewResolver;
+    }
 }
